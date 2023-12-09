3f8054515ca29c071d3b759749ab6e170cb624db
Icaro <icarovscarvalho@gmail.com>
2023-12-09 12:32:58 -0300

    Alteração das funcionalidades da interface e atualização dos elementos dentro da interface.
    - Foi feita a transformação na forma como a interface executava a abertura, para evitar problemas de cruzamento de códigos em relação ao textContent.
    
    - Dois elementos foram adicionados para funcionamento da interface. Agora um elemento é responsável apenas pela abertura e fechamento, enquanto o outro elemento carrega as informações da interface com utilização do display block e display none.
    
    - O código das pokebolas dentro da interface, foi alterado para conseguir aplicar interação aos elementos. Código ainda necessita de mais revisões de funcionalidade.
    
    - Variáveis dinâmicas adicionadas foram na img(pokebola) e span(spanChangeCircle)

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,110 +1,157 @@
-const asideInterface = document.getElementById('interface')
+const interfaceGroup = document.getElementById('interfaceGroup')
+const btnOpenClose = document.getElementById('btnOpenClose')
+const interface = document.getElementById('interface')
 
 const skinPokeball = {
-    pokeball: 'Pokebola.png',
-    greatball: 'Greatball.png',
-    ultraballball: 'Ultraball.png',
-    masterball: 'Masterball.png'   
+    pkball: 'Pokebola.png',
+    gtball: 'Greatball.png',
+    utball: 'Ultraball.png',
+    mtball: 'Masterball.png'   
 }
 
-const interfacePokeball = document.querySelectorAll('#interface img')
-
-const asideInterfaceClose = document.getElementById('escInterface')
-
-asideInterface.addEventListener('mouseover', () => {
-    asideInterface.style.animation = '1s linear 0s infinite alternate asideMoveShow'
-})
-
-asideInterface.addEventListener('mouseout', () => {
-    asideInterface.style.animation = 'none'
-})
-
-//FUNÇÃO PARA ABRIR A GUIA DA INTERFACE
-asideInterface.addEventListener('click', ()=> {
-
-    asideInterface.addEventListener('mouseover', () => {
-    asideInterface.style.animation = 'none'
+//====>====>====>====>====>====>====>====>====>
+//MOVIMENTAÇÃO DO BOTÃO DE INTERFACE (HABILTAR/DESABILITAR)
+function mouseOver() {
+    btnOpenClose.addEventListener('mouseover', () => {
+        btnOpenClose.style.animation = '1s linear 0s infinite alternate asideMoveShow'
     })
+}
+mouseOver()
 
-    asideInterface.style.borderRadius = '10px';
-    asideInterface.style.padding = '50px 20px'
-    asideInterface.style.height = '500px';
-    asideInterface.style.width = '200px';
-    asideInterface.style.flexDirection = 'column';
-    asideInterface.style.justifyContent = 'space-between'
-    asideInterface.style.alignItems = 'center';
-    asideInterface.style.animation = 'none'
+function mouseOut() {
+    btnOpenClose.addEventListener('mouseout', () => {
+        btnOpenClose.style.animation = 'none'
+    })
+}
+mouseOut()
 
+//====>====>====>====>====>====>====>====>====>
+//FUNÇÃO DE CRIAÇÃO DA INTERFACE
+function interfaceOpenClose() {
+    if(blockVerifyInstructions == 'close'){
+        blockVerifyInstructions = 'open'
+        console.log(blockVerifyInstructions)
+        //#PARAR o Hover nos asides após ABRIR a barra de aside
+        btnOpenClose.addEventListener('mouseover', ()=>{
+            btnOpenClose.style.animation = 'none'
+        })
+
+        btnOpenClose.textContent = 'x'
+        btnOpenClose.style.width = '25px'
+        btnOpenClose.style.height = '25px'
+        btnOpenClose.style.background = 'none'
+        btnOpenClose.style.backgroundColor = '#ee3434'
+        btnOpenClose.style.color = '#ffffff'
+        btnOpenClose.style.margin = '-35px 0px 10px 0px'
+
+        interfaceGroup.style.display = 'flex'
+        interfaceGroup.style.flexDirection = 'column'
+        interfaceGroup.style.alignItems = 'flex-end'
+        interfaceGroup.style.justifyContent = 'center'
+
+        interface.style.display = 'block'
+
+        interface.style.borderRadius = '10px';
+        interface.style.padding = '0px 20px'
+        interface.style.height = '500px';
+        interface.style.width = '200px';
+        interface.style.flexDirection = 'column';
+        interface.style.justifyContent = 'space-between'
+        interface.style.alignItems = 'center'
+        interface.style.animation = 'none' 
+
+        interface.style.transition = '.5s ease-in-out .5s';
+
+    }else{
+        blockVerifyInstructions = 'close'
+        console.log(blockVerifyInstructions)
+
+        //#VOLTAR o Hover nos asides após FECHAR a barra de aside
+        btnOpenClose.addEventListener('mouseover', ()=>{
+            btnOpenClose.style.animation = '1s linear 0s infinite alternate asideMoveShow'
+        })
+
+        interface.style.display = 'none'
+
+        btnOpenClose.style.borderRadius = '50%';
+        btnOpenClose.style.padding = '0px'
+        btnOpenClose.style.height = '70px';
+        btnOpenClose.style.width = '70px';
+        btnOpenClose.style.flexDirection = '';
+        btnOpenClose.style.justifyContent = 'center'
+        btnOpenClose.style.alignItems = 'center'
+        btnOpenClose.style.backgroundColor = '#fff'
+        btnOpenClose.style.color = '#000'
+        
+        btnOpenClose.textContent = ''
+        
+        const span = document.createElement('span')
+        span.textContent = 'menu'
+        span.classList.add('material-symbols-outlined')
+        btnOpenClose.appendChild(span)
+    
+        interface.style.transition = '.5s';
+    }
+}
+
+//CHAMADA DA FUNÇÃO DE ABRE E FECHA INTERFACE
+btnOpenClose.addEventListener('click', () => {
+    interfaceOpenClose()
+})
+//====>====>====>====>====>====>====>====>====>
+//FUNÃO DE CRIAÇÃO DAS POKEBOLAS
+const createPokeballs= () => {
+    
+    // Criando os elementos HTML
+    const section = document.createElement('section');
+    const div = document.createElement('div');
+    const spanStar = document.createElement('span');
+    const spanChangeCircle = document.createElement('span');
+    const img = document.createElement('img');
+    img.classList.add('pokeballInterface')
     
 
+    // Adicionando classes e atributos conforme necessário
+    spanStar.classList.add('material-symbols-outlined');
+    spanChangeCircle.classList.add('material-symbols-outlined');
 
-    asideInterface.innerHTML = ''
-    for(let i = 0; i < 3; i++){
-        // Criando os elementos HTML
-        const section = document.createElement('section');
-        const div = document.createElement('div');
-        const spanStar = document.createElement('span');
-        const spanChangeCircle = document.createElement('span');
-        const img = document.createElement('img');
-        img.classList.add('pokeballInterface')
-        
+    spanStar.textContent = 'star';
+    spanChangeCircle.textContent = 'change_circle';
 
-        // Adicionando classes e atributos conforme necessário
-        spanStar.classList.add('material-symbols-outlined');
-        spanChangeCircle.classList.add('material-symbols-outlined');
+    img.src = `styles/media/${skinPokeball.pkball}`;
+    img.alt = 'Favorits Slot';
 
-        spanStar.textContent = 'star';
-        spanChangeCircle.textContent = 'change_circle';
+    // Estruturando os elementos
+    div.appendChild(spanStar);
+    div.appendChild(spanChangeCircle);
 
-        img.src = `styles/media/${skinPokeball.pokeball}`;
-        img.alt = 'Favorits Slot';
+    section.appendChild(div);
+    section.appendChild(img);
 
-        // Estruturando os elementos
-        div.appendChild(spanStar);
-        div.appendChild(spanChangeCircle);
+    // Adicionando a seção ao elemento asideInterface
+    interface.appendChild(section);
 
-        section.appendChild(div);
-        section.appendChild(img);
+    //=>=>=>=>=>==>===>====>=====>======>========>==========>====>
+    //INTERAÇÃO COM A POKEBOLA - FAVORITAR
+    img.style.cursor = 'pointer'
+    img.onclick = clickPokeballInsiderInterface
+    function clickPokeballInsiderInterface(){
+        console.log('gotcha')
+        img.style.rotate = '720deg'
+        img.style.transition = '.5s'
+    }
+    //=>=>=>=>=>==>===>====>=====>======>========>==========>====>
+    //MUDAR SKIN DA POKEBOLA
+    const skins = [skinPokeball.pkball, skinPokeball.gtball, skinPokeball.utball ,skinPokeball.mtball]
 
-        // Adicionando a seção ao elemento asideInterface
-        asideInterface.appendChild(section);
-
-        section.addEventListener('click', () => {
-            img.style.backgroundColor = 'tomato'
-        })
+    spanChangeCircle.style.cursor = 'pointer'
+    spanChangeCircle.onclick = changePokeball
+    function changePokeball() {
+        const masterball = img.src = `styles/media/${skins[2]}`
     }
 
-    asideInterfaceClose.style.display = 'block'
+    return section;
+}
 
-    asideInterface.style.transition = '.5s';
-
-})
-
-//FUNÇÃO PARA FECHAR A GUIA DA INTERFACE
-asideInterfaceClose.addEventListener('click', () => {
-
-    asideInterface.addEventListener('mouseover', () => {
-    asideInterface.style.animation = '1s linear 0s infinite alternate asideMoveShow'
-    })
-
-    asideInterfaceClose.style.display = 'none'
-
-    asideInterface.style.borderRadius = '50%';
-    asideInterface.style.padding = '0px'
-    asideInterface.style.height = '70px';
-    asideInterface.style.width = '70px';
-    asideInterface.style.flexDirection = '';
-    asideInterface.style.justifyContent = 'center'
-    asideInterface.style.alignItems = 'center'
-
-    asideInterface.textContent = ''
-
-    const span = document.createElement('span')
-    span.textContent = 'menu'
-    span.classList.add('material-symbols-outlined')
-    asideInterface.appendChild(span)
-
-    asideInterface.style.transition = '.5s';
-})
-
-//FUNÇÃO DENTRO DA GUIA DA INTERFACE
+const [slotBase1, slotBase2, slotBase3] = [createPokeballs(),createPokeballs(),createPokeballs()]
+//====>====>====>====>====>====>====>====>====>
